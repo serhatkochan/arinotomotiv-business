c584ca3f0b79b432d48ed4c02841a79560010f48
Serhat Koçhan <serhatkochan@hotmail.com.tr>
2023-10-01 15:04:54 +0300

    whatsappp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/whatsappLogo.png'
 import './App.css'
 
 function App() {
@@ -8,7 +8,12 @@ function App() {
 
   return (
     <>
-      <h1>Çok Yakında Web Sitemiz Hizmetinizde...</h1>
+      <h1>Çok Yakında Web Sitemiz Hizmetinizde!</h1>
+      <div className="card">
+          <a href="https://wa.me/5433451076" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+      </div>
     </>
   )
 }
